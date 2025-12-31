598fef356b04bfb9ca46de39ed6964761b6ad8d0
adarshAD6394 <adarsh.gupta.21C@iitram.ac.in>
2025-12-31 09:50:50 +0000

    Zero chapter

---
+++ exercises/lesson_2/target/debug/deps/lesson_2-be71908383973dff.d
@@ -0,0 +1,5 @@
+/mnt/d/projects/Rust/exercises/lesson_2/target/debug/deps/lesson_2-be71908383973dff.d: src/main.rs
+
+/mnt/d/projects/Rust/exercises/lesson_2/target/debug/deps/lesson_2-be71908383973dff: src/main.rs
+
+src/main.rs:
---
+++ exercises/lesson_2/target/debug/lesson_2.d
@@ -0,0 +1,1 @@
+/mnt/d/projects/Rust/exercises/lesson_2/target/debug/lesson_2: /mnt/d/projects/Rust/exercises/lesson_2/src/main.rs
