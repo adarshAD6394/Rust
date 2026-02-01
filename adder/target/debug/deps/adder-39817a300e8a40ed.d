b3615358746201d5ae82a8693f32064e5dd68512
adarshAD6394 <adarsh.gupta.21C@iitram.ac.in>
2026-02-01 06:55:33 +0000

    Testing in Rust

---
+++ adder/target/debug/deps/adder-39817a300e8a40ed.d
@@ -0,0 +1,7 @@
+/mnt/d/Projects/Rust/adder/target/debug/deps/adder-39817a300e8a40ed.d: src/lib.rs
+
+/mnt/d/Projects/Rust/adder/target/debug/deps/libadder-39817a300e8a40ed.rlib: src/lib.rs
+
+/mnt/d/Projects/Rust/adder/target/debug/deps/libadder-39817a300e8a40ed.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-98694191895e735e.d
@@ -0,0 +1,5 @@
+/mnt/d/Projects/Rust/adder/target/debug/deps/adder-98694191895e735e.d: src/lib.rs
+
+/mnt/d/Projects/Rust/adder/target/debug/deps/adder-98694191895e735e: src/lib.rs
+
+src/lib.rs:
