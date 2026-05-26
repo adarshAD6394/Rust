508268a3b129d66336dd0077f4ce886d7a4de658
adarshAD6394 <adarsh.gupta.21C@iitram.ac.in>
2026-05-26 19:58:56 +0000

    prblm 242

--- strings/src/bin/easy_prblm242_valid_anagram.rs
+++ strings/src/bin/easy_prblm242_valid_anagram.rs
@@ -17,23 +17,43 @@ fn main() {
     println!("result : {}", is_anagram(&s , &t));
 }
 
-fn is_anagram (s : &str, t: &str) -> bool {
+// fn is_anagram (s : &str, t: &str) -> bool {
+//     if s.len() != t.len() {
+//         return false;
+//     }
+
+//     let mut h1: HashMap<char, i32> = HashMap::new();
+//     let mut h2: HashMap<char, i32> = HashMap::new();
+
+//     for c1 in s.chars() {
+//         let count = h1.entry(c1).or_insert(0);
+//         *count += 1;
+//     }
+
+//     for c2 in t.chars(){
+//         let count = h2.entry(c2).or_insert(0);
+//         *count += 1;
+//     }
+
+//     return h1 == h2;
+// }
+
+fn is_anagram (s: &str, t: &str) -> bool {
     if s.len() != t.len() {
         return false;
     }
-
-    let mut h1: HashMap<char, i32> = HashMap::new();
-    let mut h2: HashMap<char, i32> = HashMap::new();
-
-    for c1 in s.chars() {
-        let count = h1.entry(c1).or_insert(0);
+    let mut map : HashMap<char, i32> = HashMap::new();
+    for c in s.chars() {
+        let count = map.entry(c).or_insert(0);
         *count += 1;
     }
 
-    for c2 in t.chars(){
-        let count = h2.entry(c2).or_insert(0);
-        *count += 1;
+    for c in t.chars() {
+        let count = map.entry(c).or_insert(0);
+        *count -= 1;
+        if *count < 0 {
+            return false;
+        }
     }
-
-    return h1 == h2;
+    true
 }
